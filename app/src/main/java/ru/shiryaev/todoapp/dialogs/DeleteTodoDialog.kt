2a1841b0145ce376117cf4b7e1552b80d56c9f32
ShiryaevNikolay <deendeeps@list.ru>
2020-12-11 17:27:18 +0300

    Переместил диалог удаления заментки в низ экрана и добавил отступы от экрана

--- app/src/main/java/ru/shiryaev/todoapp/dialogs/DeleteTodoDialog.kt
+++ app/src/main/java/ru/shiryaev/todoapp/dialogs/DeleteTodoDialog.kt
@@ -1,13 +1,13 @@
 package ru.shiryaev.todoapp.dialogs
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.DialogFragment
-import ru.shiryaev.todoapp.R
 import ru.shiryaev.todoapp.databinding.DialogTodoMenuBinding
 import ru.shiryaev.todoapp.models.Todo
+
 
 class DeleteTodoDialog(private val todo: Todo) : DialogFragment() {
 
@@ -21,12 +21,22 @@ class DeleteTodoDialog(private val todo: Todo) : DialogFragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = DialogTodoMenuBinding.inflate(inflater, container, false)
-        dialog?.window?.setBackgroundDrawableResource(R.drawable.dialog_bg)
         binding.deleteBtn.setOnClickListener {
             onCLickBtn?.invoke(todo)
             dismiss()
         }
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val p = dialog!!.window!!.attributes
+        p.width = ViewGroup.LayoutParams.MATCH_PARENT
+        dialog!!.window!!.apply {
+            attributes = p
+            setGravity(Gravity.BOTTOM)
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+        }
     }
 
     override fun onDestroyView() {
